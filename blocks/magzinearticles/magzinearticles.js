f8a42934dfa24e529c5c2f06399b1a427411200b
Mitesh Sanjay. Arora <433740@conneqtcorp.com>
2024-11-12 09:40:04 +0530

    css styling for many parts

--- blocks/magzinearticles/magzinearticles.js
+++ blocks/magzinearticles/magzinearticles.js
@@ -26,22 +26,24 @@ articleData.forEach((d)=>{
     pict.append(img) 
     h5.innerHTML = d.title;
     desc.innerHTML = d.description;
-    // div1.append(pict);
     div2.append(h5);
     div2.append(desc);
-    // const path = 'https://wknd.site/' + d.image;
     const pictureElement = createOptimizedPicture(d.image, d.title);
     div1.appendChild(pictureElement);
-
     li.append(div1);
     li.append(div2);
     uli.append(li);
+
+    [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+        else div.className = 'cards-card-body';
+      });
 })
-const li = uli.querySelector('li');
-[...li.children].forEach((div) => {
-    if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-    else div.className = 'cards-card-body';
-  });
+// const li = uli.querySelector('li');
+// [...li.children].forEach((div) => {
+//     if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+//     else div.className = 'cards-card-body';
+//   });
   uli.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(uli);
